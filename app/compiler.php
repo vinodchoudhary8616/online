a20ae515a42bb5c5d9cfc23f66f706996be7b71b
vinodchoudhary8616 <yukthivinodchoudhary@gmail.com>
2022-11-22 07:10:33 +0530

    first commit

---
+++ app/compiler.php
@@ -0,0 +1,26 @@
+<?php
+    $language = strtolower($_POST['language']);
+    $code = $_POST['code'];
+
+    $random = substr(md5(mt_rand()), 0, 7);
+    $filePath = "temp/" . $random . "." . $language;
+    $programFile = fopen($filePath, "w");
+    fwrite($programFile, $code);
+    fclose($programFile);
+    if($language == "python") {
+        $output = shell_exec("C:\Users\VINOD CHOUDHARY\AppData\Local\Programs\Python\Python39");
+        echo $output;
+    }
+    if($language == "c") {
+        $outputExe = $random . ".exe";
+        shell_exec("gcc $filePath -o $outputExe");
+        $output = shell_exec(__DIR__ . "//$outputExe");
+        echo $output;
+    }
+    if($language == "cpp") {
+        $outputExe = $random . ".exe";
+        shell_exec("g++ $filePath -o $outputExe");
+        $output = shell_exec(__DIR__ . "//$outputExe");
+        echo $output;
+    }
+?>
